3a47e3851a40ff94651e883bef24e62afccf6984
Sean Hickman <sean@grumply.com>
2020-02-13 23:38:00 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, deepseq, pure-json, pure-txt, unordered-containers, vector, containers, pure-txt-trie, stdenv }:
 mkDerivation {
   pname = "pure-variance";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base deepseq pure-json pure-txt unordered-containers containers pure-txt-trie vector ];
   homepage = "github.com/grumply/pure-variance";
